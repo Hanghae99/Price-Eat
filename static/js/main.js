acfc772829dbc221ac465c005cd3838ad0e4d18d
JaejinLee <jaejin1112@gmail.com>
2022-03-10 18:35:40 +0900

    final commit

--- static/js/main.js
+++ static/js/main.js
@@ -18,27 +18,27 @@ function getInputValue() {
   }
 }
 
-function show_store() {
-  $.ajax({
-    type: "GET",
-    url: "/store",
-    data: {},
-    success: function (response) {
-      console.log(response);
-      let rows = response["stores_list"];
-      for (let i = 0; i < rows.length; i++) {
-        let name = rows[i]["name"];
-        let address = rows[i]["address"];
-        let store_img = rows[i]["img"];
-        let temp_html = `<div class="desc_list" onclick="location.href='/store_desc?name=${name}'">
-                          <div class="store_img" style="background-image: url(${store_img});">
-                          </div>
-                          <div class="store_name">
-                            ${name}
-                          </div>
-                        </div>`;
-        $("#list_box").append(temp_html);
-      }
-    },
-  });
-}
+// function show_store() {
+//   $.ajax({
+//     type: "GET",
+//     url: "/store",
+//     data: {},
+//     success: function (response) {
+//       console.log(response);
+//       let rows = response["stores_list"];
+//       for (let i = 0; i < rows.length; i++) {
+//         let name = rows[i]["name"];
+//         let address = rows[i]["address"];
+//         let store_img = rows[i]["img"];
+//         let temp_html = `<div class="desc_list" onclick="location.href='/store_desc?name=${name}'">
+//                           <div class="store_img" style="background-image: url(${store_img});">
+//                           </div>
+//                           <div class="store_name">
+//                             ${name}
+//                           </div>
+//                         </div>`;
+//         $("#list_box").append(temp_html);
+//       }
+//     },
+//   });
+// }
