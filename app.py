d3c05f412625fe9aeecb801eb38debb70815a771
JaejinLee <jaejin1112@gmail.com>
2022-03-10 11:54:18 +0900

    add @app route search

--- app.py
+++ app.py
@@ -130,6 +130,10 @@ def reviews_get():
     review_list = list(db.reviews.find({}, {'_id': False}))
     return jsonify({'reviews': review_list})
 
+@app.route("/search_desc")
+def search_desc():
+    return render_template('search.html') 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
